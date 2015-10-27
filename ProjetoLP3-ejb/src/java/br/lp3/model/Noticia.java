c81dbcace4e1929ff24a4293e2da3876c20e62d3
31409695 <31409695@093129-P31S302.macklabs.br>
2015-10-27 12:02:07 -0200

    Initial commit

---
+++ ProjetoLP3-ejb/src/java/br/lp3/model/Noticia.java
@@ -0,0 +1,82 @@
+package br.lp3.model;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author 31409695
+ */
+public class Noticia implements Serializable {
+    
+    private long gid;
+    private String title;
+    private String url;
+    private String author;
+    private String content;
+    private long timestamp;
+
+    public Noticia(long gid, String title, String url, String author, String content, long timestamp) {
+        this.gid = gid;
+        this.title = title;
+        this.url = url;
+        this.author = author;
+        this.content = content;
+        this.timestamp = timestamp;
+    }
+
+    public Noticia() {
+    }
+
+    public long getGid() {
+        return gid;
+    }
+
+    public void setGid(long gid) {
+        this.gid = gid;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    @Override
+    public String toString() {
+        return "Noticia{" + "gid=" + gid + ", title=" + title + ", url=" + url + ", author=" + author + ", content=" + content + ", timestamp=" + timestamp + '}';
+    }
+}
